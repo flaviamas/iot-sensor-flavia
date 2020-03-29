a51cc1cab53e66f3dce46d24642f5ba7e83f05a8
Flavia Masoni <flavia_ma@hotmail.it>
2020-03-29 18:24:26 +0200

    delete file

--- connectionstring.py
+++
@@ -1,3 +0,0 @@
-
-connect1 = "HostName=assignment-hub.azure-devices.net;DeviceId=mydeviceid1;SharedAccessKey=9NxJ78AWhU5xGed9BDpCV2tNZeQ9XVgpZK9ObFBFEOk="
-connect2 = "HostName=assignment-hub.azure-devices.net;DeviceId=mydevice2;SharedAccessKey=h3mGZrP1HR7vXqX7GCoIlwsjM4UhGq5v0n4p9mpsX38="
