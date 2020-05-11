1d962089a5d3787a81897d45e27706d082c27fc4
Flavia Masoni <flavia_ma@hotmail.it>
2020-05-11 18:58:02 +0200

    created serverjs and add express

---
+++ Assignment4/server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
