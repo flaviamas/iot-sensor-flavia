065b1c96c3161499027d63b0d4d0bd0cdf6bb2fc
Flavia Masoni <flavia_ma@hotmail.it>
2020-05-11 19:40:37 +0200

    index created

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var fs = require('fs')
 var https = require('https')
+var path=require('path')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
 
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+//create a server
 https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
@@ -14,3 +18,5 @@ https.createServer({
 .listen(3000, function () {
   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
 })
+
+
