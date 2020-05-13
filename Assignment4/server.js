4aa3b590b30407d8556ed9a6a48f3e0d4a71d885
Flavia Masoni <flavia_ma@hotmail.it>
2020-05-13 12:58:51 +0200

    ass4 start

---
+++ Assignment4/server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+var app = express()
+var path = require("path")
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
