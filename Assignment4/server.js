a77ac00da7f90bf4709b32b1f340b5c9ed8fd1cc
Flavia Masoni <flavia_ma@hotmail.it>
2020-05-14 22:03:38 +0200

    changes to sensors fetch

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -1,33 +1,12 @@
-var express = require('express')
-var fs = require('fs')
-var https = require('https')
-var app = express()
-var path = require("path")
-var bodyParser = require('body-parser');
-var stringc = require('./connectionstr')
-
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(3000, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-})
-
-
-var Protocol = require('azure-iot-device-mqtt').Mqtt;
-// Uncomment one of these transports and then change it in fromConnectionString to test other transports
-// var Protocol = require('azure-iot-device-amqp').AmqpWs;
-// var Protocol = require('azure-iot-device-http').Http;
-// var Protocol = require('azure-iot-device-amqp').Amqp;
-// var Protocol = require('azure-iot-device-mqtt').MqttWs;
-var Client = require('azure-iot-device').Client;
-var Message = require('azure-iot-device').Message;
+var express = require("express");
+var fs = require("fs");
+var https = require("https");
+var app = express();
+var path = require("path");
+var stringc = require("./connectionstr");
+var Protocol = require("azure-iot-device-mqtt").Mqtt;
+var Client = require("azure-iot-device").Client;
+var Message = require("azure-iot-device").Message;
 
 var deviceConnectionString = stringc.fcstring;
 
@@ -36,37 +15,19 @@ var client = Client.fromConnectionString(deviceConnectionString, Protocol);
 
 var connectCallback = function (err) {
   if (err) {
-    console.error('Could not connect: ' + err.message);
+    console.error("Could not connect: " + err.message);
   } else {
-    console.log('Client connected');
-    client.on('message', function (msg) {
-      console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
-      // When using MQTT the following line is a no-op.
-      client.complete(msg, printResultFor('completed'));
-      // The AMQP and HTTP transports also have the notion of completing, rejecting or abandoning the message.
-      // When completing a message, the service that sent the C2D message is notified that the message has been processed.
-      // When rejecting a message, the service that sent the C2D message is notified that the message won't be processed by the device. the method to use is client.reject(msg, callback).
-      // When abandoning the message, IoT Hub will immediately try to resend it. The method to use is client.abandon(msg, callback).
-      // MQTT is simpler: it accepts the message by default, and doesn't support rejecting or abandoning a message.
+    console.log("Client connected");
+    client.on("message", function (msg) {
+      console.log("Id: " + msg.messageId + " Body: " + msg.data);
+      client.complete(msg, printResultFor("completed"));
     });
 
-    // Create a message and send it to the IoT Hub every two seconds
-    var sendInterval = setInterval(function () {
-      var windSpeed = 10 + (Math.random() * 4); // range: [10, 14]
-      var temperature = 20 + (Math.random() * 10); // range: [20, 30]
-      var humidity = 60 + (Math.random() * 20); // range: [60, 80]
-      var data = JSON.stringify({ deviceId: 'myFirstDevice', windSpeed: windSpeed, temperature: temperature, humidity: humidity });
-      var message = new Message(data);
-      message.properties.add('temperatureAlert', (temperature > 28) ? 'true' : 'false');
-      console.log('Sending message: ' + message.getData());
-      client.sendEvent(message, printResultFor('send'));
-    }, 2000);
-
-    client.on('error', function (err) {
+    client.on("error", function (err) {
       console.error(err.message);
     });
 
-    client.on('disconnect', function () {
+    client.on("disconnect", function () {
       clearInterval(sendInterval);
       client.removeAllListeners();
       client.open(connectCallback);
@@ -79,7 +40,34 @@ client.open(connectCallback);
 // Helper function to print results in the console
 function printResultFor(op) {
   return function printResult(err, res) {
-    if (err) console.log(op + ' error: ' + err.toString());
-    if (res) console.log(op + ' status: ' + res.constructor.name);
+    if (err) console.log(op + " error: " + err.toString());
+    if (res) console.log(op + " status: " + res.constructor.name);
   };
 }
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/", function (req, res) {
+  let data = req.body;
+  var message = new Message(JSON.stringify(data));
+  client.sendEvent(message, printResultFor("send"));
+  res.send("POST request to the homepage");
+});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(3000, function () {
+    console.log(
+      "Example app listening on port 3000! Go to https://localhost:3000/"
+    );
+  });
